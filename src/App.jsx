f53584bbd8301fb60bddba0383278c0da4a49002
JayChauhan2 <jayschauhan3@gmail.com>
2026-03-16 14:34:41 -0400

    + feedback form

--- src/App.jsx
+++ src/App.jsx
@@ -13,30 +13,30 @@ import OrganizationsApp from './apps/OrganizationsApp';
 import AwardsApp from './apps/AwardsApp';
 
 const APPS = [
-  { 
-    id: 'photos', name: 'Photos', 
-    icon: <img src="https://upload.wikimedia.org/wikipedia/en/5/5e/Photos_icon_for_OS_X.png" alt="Photos" className="w-full h-full object-cover" />, 
-    appBg: 'bg-black', component: PhotosApp 
+  {
+    id: 'photos', name: 'Photos',
+    icon: <img src="https://upload.wikimedia.org/wikipedia/en/5/5e/Photos_icon_for_OS_X.png" alt="Photos" className="w-full h-full object-cover" />,
+    appBg: 'bg-black', component: PhotosApp
   },
-  { 
-    id: 'contacts', name: 'Contacts', 
-    icon: <img src="https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/c3/e0/9d/c3e09d8c-fb97-f0be-8463-b011f602c53a/contacts-0-0-1x_U007epad-0-1-0-sRGB-0-85-220.png/512x512bb.jpg" alt="Contacts" className="w-full h-full object-cover" />, 
-    appBg: 'bg-gray-50', component: ContactsApp 
+  {
+    id: 'contacts', name: 'Contacts',
+    icon: <img src="https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/c3/e0/9d/c3e09d8c-fb97-f0be-8463-b011f602c53a/contacts-0-0-1x_U007epad-0-1-0-sRGB-0-85-220.png/512x512bb.jpg" alt="Contacts" className="w-full h-full object-cover" />,
+    appBg: 'bg-gray-50', component: ContactsApp
   },
-  { 
-    id: 'projects', name: 'Projects', 
-    icon: <img src="https://upload.wikimedia.org/wikipedia/commons/6/67/App_Store_%28iOS%29.svg" alt="Projects" className="w-full h-full object-cover" />, 
-    appBg: 'bg-white', component: ProjectsApp 
+  {
+    id: 'projects', name: 'Projects',
+    icon: <img src="https://upload.wikimedia.org/wikipedia/commons/6/67/App_Store_%28iOS%29.svg" alt="Projects" className="w-full h-full object-cover" />,
+    appBg: 'bg-white', component: ProjectsApp
   },
-  { 
-    id: 'organizations', name: 'Organizations', 
-    icon: <img src="https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/cf/a1/e7/cfa1e733-abf2-2a89-394f-1de4a5043743/files-0-0-1x_U007epad-0-1-0-sRGB-85-220.png/512x512bb.jpg" alt="Organizations" className="w-full h-full object-cover" />, 
-    appBg: 'bg-gray-50', component: OrganizationsApp 
+  {
+    id: 'organizations', name: 'Organizations',
+    icon: <img src="https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/cf/a1/e7/cfa1e733-abf2-2a89-394f-1de4a5043743/files-0-0-1x_U007epad-0-1-0-sRGB-85-220.png/512x512bb.jpg" alt="Organizations" className="w-full h-full object-cover" />,
+    appBg: 'bg-gray-50', component: OrganizationsApp
   },
-  { 
-    id: 'awards', name: 'Awards', 
-    icon: <img src="https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/c2/82/bc/c282bc4d-7d60-1c41-3282-1d9accecf0bd/tips-0-0-1x_U007epad-0-1-0-sRGB-85-220.png/512x512bb.jpg" alt="Tips" className="w-full h-full object-cover" />, 
-    appBg: 'bg-white', component: AwardsApp 
+  {
+    id: 'awards', name: 'Awards',
+    icon: <img src="https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/c2/82/bc/c282bc4d-7d60-1c41-3282-1d9accecf0bd/tips-0-0-1x_U007epad-0-1-0-sRGB-85-220.png/512x512bb.jpg" alt="Tips" className="w-full h-full object-cover" />,
+    appBg: 'bg-white', component: AwardsApp
   },
 ];
 
@@ -62,7 +62,7 @@ export default function App() {
   const statusTheme = (!currentApp) ? 'dark' : (isDarkApp ? 'dark' : 'light');
 
   return (
-    <div 
+    <div
       className="w-full h-screen flex items-center justify-center relative overflow-hidden bg-cover bg-center"
       style={{ backgroundImage: "url('https://images.pexels.com/photos/129731/pexels-photo-129731.jpeg?auto=compress&cs=tinysrgb&w=2000')" }}
     >
@@ -71,7 +71,7 @@ export default function App() {
 
       <IPadFrame onHomeClick={handleHomeButton}>
         <StatusBar theme={statusTheme} />
-        
+
         {/* Subtle Status Bar Gradient for readability */}
         <div className="absolute top-0 inset-x-0 h-24 bg-gradient-to-b from-black/40 to-transparent pointer-events-none z-40"></div>
 
@@ -106,8 +106,8 @@ export default function App() {
           {isLocked ? (
             <LockScreen key="lock" onUnlock={() => setIsLocked(false)} />
           ) : (
-            <motion.div 
-              key="home" 
+            <motion.div
+              key="home"
               className="absolute inset-0 pt-8 px-8 z-10"
               initial={{ opacity: 0, scale: 0.95 }}
               animate={{ opacity: 1, scale: 1 }}
@@ -140,6 +140,15 @@ export default function App() {
           )}
         </AnimatePresence>
       </IPadFrame>
+
+      <a
+        href="https://docs.google.com/forms/d/e/1FAIpQLSc-VHqQXwk_rHPsY4g-y_Lc5JXIpxB-iJZoLmWhaZXSlpJEHg/viewform"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="fixed bottom-4 right-6 text-[11px] text-white/80 hover:text-white/100 transition-all duration-300 z-[100] font-medium tracking-tight no-underline select-none"
+      >
+        Have feedback? Share it here
+      </a>
     </div>
   );
 }
